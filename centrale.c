0b1ce33e18aac352744e3daed4702361f7485c27
ALEXSNOW16 <105298784+ALEXSNOW16@users.noreply.github.com>
2022-11-26 09:58:53 +0100

    Update centrale.c

--- centrale.c
+++ centrale.c
@@ -21,6 +21,9 @@ void centrale_liberer(t_centrale* c)
 }
 
 void centrale_distribuer(t_centrale* centrale,t_habitation* habitation,int quantitee)
+/*
+Fonction qui sert à distribuer de l'electricité à une habitation
+*/
 {
     habitation->id_centrale_fournisseuse.caseX = centrale->id_centrale.caseX; // on recopie l'ID de la centrale
     habitation->id_centrale_fournisseuse.caseY = centrale->id_centrale.caseY; // on recopie l'ID de la centrale
@@ -32,7 +35,8 @@ int centrale_place_libre(int col,int lig,t_case*** kase)
 {
     int i,j;
     int libre=1;
-    if(!centrale_depassement_matrice(col,lig))
+    // On regarde si on sort de la grille
+    if(!centrale_depassement_matrice(col,lig)) // Si condition vrai (libre) on regarde, en parcourant case par case, si il n'y a pas deja un objet construit 
     {
         for(i=lig;i<lig+CENTRALE_H;i++)
         {
@@ -45,11 +49,14 @@ int centrale_place_libre(int col,int lig,t_case*** kase)
             }
         }
     }
-    else libre=0;
+    else libre=0; //Sinon pas libre
     return libre;
 }
 
 int centrale_depassement_matrice(int colonne,int ligne)
+/*
+Fonction qui vérifie si on dépasse la grille(sord du bord)
+*/
 {
     int depasse=1;
     if((colonne>=0)&&(colonne<=NB_CASES_COL-CENTRALE_W)&&(ligne>=0)&&(ligne<=NB_CASES_LIG-CENTRALE_H))
@@ -60,6 +67,10 @@ int centrale_depassement_matrice(int colonne,int ligne)
 }
 
 void centrale_placer(t_centrale* centrale,int col,int lig,t_case*** kase)
+/*
+Fonction qui place une centrale(si toutes les conditions sont remplis)
+*/
+
 {
     int i,j;
     centrale->Pos_X=col;
@@ -70,12 +81,13 @@ void centrale_placer(t_centrale* centrale,int col,int lig,t_case*** kase)
     {
         for(j=col;j<col+CENTRALE_W;j++)
         {
-            case_remplir(kase[i][j],CENTRALE,centrale);
+            case_remplir(kase[i][j],CENTRALE,centrale); //on met toutes les cases de type centrale
         }
     }
 }
 
 void centrale_afficher(t_centrale* centrale,int niveau)
+//Affichage
 {
     if(niveau==NIVEAU_EAU)
     {
