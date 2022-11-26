113bbb65cc3f897dc44d8dcf04f0eacded28a336
ALEXSNOW16 <105298784+ALEXSNOW16@users.noreply.github.com>
2022-11-26 09:35:13 +0100

    Update case.c

--- case.c
+++ case.c
@@ -3,17 +3,26 @@
 
 void case_vider(t_case* k)
 {
+    /*
+    Fonction qui permet de libérer(vider une case) ie: qui met à vide ses atributs
+    */
     k->elem=NULL;
     k->type=VIDE;
 }
 
 void case_remplir(t_case* k,int type,void* elem)
 {
+    /*
+    Fonction qui permet de remplir une case c'est à dire d'attribuer un type donné à une case
+    */
     k->type=type;
     k->elem=elem;
 }
 
 t_case* case_allouer()
+    /*
+    Fonction qui permet d'allouer une case de type vide
+    */
 {
     t_case* nouv;
     nouv=malloc(sizeof(t_case));
@@ -24,6 +33,9 @@ t_case* case_allouer()
 }
 
 int case_libre(t_case* kase)
+    /*
+    Fonction qui teste si une case est libre: 0 si vide et 1 sinon
+    */
 {
     int libre=1;
 
@@ -35,6 +47,9 @@ int case_libre(t_case* kase)
 }
 
 void case_free(t_case* k)
+    /*
+    Fonction qui permet de supprimer une case
+    */
 {
     free(k);
 }
